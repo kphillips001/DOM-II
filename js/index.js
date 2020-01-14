2d7ee0a0df1b8c2def5edada9f6e554499a3cd4d
Kevin <knphillips01@gmail.com>
2020-01-14 14:59:43 -0600

    finished mouseover event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+//Mouseover
+document.querySelectorAll("a").forEach(element => {
+  element.addEventListener("mouseover",() => {
+    element.style.color= "red";
+  })
+});
