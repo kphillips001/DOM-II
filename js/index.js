a3d2c555f3d59fb949f10b01ff6a9cc1c5a4982b
Kevin <knphillips01@gmail.com>
2020-01-14 16:18:48 -0600

    added keydown event listener #5

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 // Your code goes here
 
-//#1 
+//#1 Mouseover a red
 document.querySelectorAll('a').forEach(element => {
   element.addEventListener('mouseover',() => {
     element.style.color= 'red';
   })
 });
 
-//#2 
+//#2 DoubleClick h2 Orange
 document.querySelectorAll('h2').forEach(element => {
   element.addEventListener('dblclick',() => {
     element.style.color= 'orange';
   })
 });
 
-//#3 
+//#3 Mouse Enter/Leave
  const funbus = document.querySelector('.intro img');
 
  funbus.addEventListener('mouseenter', () => {
@@ -26,9 +26,14 @@ document.querySelectorAll('h2').forEach(element => {
    funbus.style.transition = 'transform 0.2s';
  });
 
- //#4
+ //#4 Click Footer
  const footer = document.querySelector('footer');
-
  footer.addEventListener('click', () => {
-     footer.style.backgroundColor = 'red';
+  footer.style.backgroundColor = 'red';
 });
+
+//#5 KeyDown All
+const island = document.querySelector("*")
+document.addEventListener('keydown', ()  => {
+  island.style.color= "blue";
+})
