3d45f276f0a6d05ad224a8a13a95916d11394abb
Kevin <knphillips01@gmail.com>
2020-01-14 15:37:50 -0600

    added mouse enter and leave event listener

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
 // Your code goes here
 
-//Mouseover
+//#1 - Mouseever
 document.querySelectorAll("a").forEach(element => {
   element.addEventListener('mouseover',() => {
     element.style.color= "red";
   })
 });
 
-// Double Click
+//#2 - Double Click
 document.querySelectorAll("h2").forEach(element => {
   element.addEventListener("dblclick",() => {
     element.style.color= "orange";
   })
 });
+
+//#3 Mouse Enter and Leave 
+ const funbus = document.querySelector(".intro img");
+
+ funbus.addEventListener("mouseenter", () => {
+   funbus.style.transform = "scale(1.2)";
+   funbus.style.transition = "transform 0.2s";
+ });
+ funbus.addEventListener("mouseleave", () => {
+   funbus.style.transform = "scale(1)";
+   funbus.style.transition = "transform 0.2s";
+ });
+ 
