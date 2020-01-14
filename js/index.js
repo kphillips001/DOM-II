c8a0078792323c1cdc9ff46062374cc56e83e03e
Kevin <knphillips01@gmail.com>
2020-01-14 15:04:32 -0600

    added dblclick event listener

--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@
 
 //Mouseover
 document.querySelectorAll("a").forEach(element => {
-  element.addEventListener("mouseover",() => {
+  element.addEventListener('mouseover',() => {
     element.style.color= "red";
+  })
+});
+
+// Double Click
+document.querySelectorAll("h2").forEach(element => {
+  element.addEventListener("dblclick",() => {
+    element.style.color= "orange";
   })
 });
