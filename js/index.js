1e0b138db5071db48736279113615fffc7710e2c
Kevin <knphillips01@gmail.com>
2020-01-14 15:51:27 -0600

    finished #4

--- js/index.js
+++ js/index.js
@@ -1,28 +1,34 @@
 // Your code goes here
 
-//#1 - Mouseever
-document.querySelectorAll("a").forEach(element => {
+//#1 
+document.querySelectorAll('a').forEach(element => {
   element.addEventListener('mouseover',() => {
-    element.style.color= "red";
+    element.style.color= 'red';
   })
 });
 
-//#2 - Double Click
-document.querySelectorAll("h2").forEach(element => {
-  element.addEventListener("dblclick",() => {
-    element.style.color= "orange";
+//#2 
+document.querySelectorAll('h2').forEach(element => {
+  element.addEventListener('dblclick',() => {
+    element.style.color= 'orange';
   })
 });
 
-//#3 Mouse Enter and Leave 
- const funbus = document.querySelector(".intro img");
+//#3 
+ const funbus = document.querySelector('.intro img');
 
- funbus.addEventListener("mouseenter", () => {
-   funbus.style.transform = "scale(1.2)";
+ funbus.addEventListener('mouseenter', () => {
+   funbus.style.transform = 'scale(1.2)';
    funbus.style.transition = "transform 0.2s";
  });
- funbus.addEventListener("mouseleave", () => {
-   funbus.style.transform = "scale(1)";
-   funbus.style.transition = "transform 0.2s";
+ funbus.addEventListener('mouseleave', () => {
+   funbus.style.transform = 'scale(1)';
+   funbus.style.transition = 'transform 0.2s';
  });
- 
+
+ //#4
+ const footer = document.querySelector('footer');
+
+ footer.addEventListener('click', () => {
+     footer.style.backgroundColor = 'red';
+});
